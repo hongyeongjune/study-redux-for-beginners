bc34ab610bf94329065c308bf2b50193f9bfc33e
hongyeongjune <hongyeongjune@naver.com>
2022-10-03 13:43:48 +0900

    Vanilla Counter

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const text = document.querySelector("span");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let count = 0;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+text.innerText = count;
+
+const updateText = () => {
+    text.innerText = count;
+}
+
+add.addEventListener("click", () => {
+    count += 1;
+    updateText();
+})
+
+minus.addEventListener("click", () => {
+    count -= 1;
+    updateText();
+})
