20dbc4bc275952223a60c24c2ed71c2136d6eb01
hongyeongjune <hongyeongjune@naver.com>
2022-10-08 20:20:33 +0900

    Actions

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -4,10 +4,23 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const text = document.querySelector("span");
 
-const reducer = (state) => {
+const reducer = (state = 0, actions) => {
+    if (actions.type === 'ADD') {
+        return state + 1;
+    } else if (actions.type === 'MINUS') {
+        return state - 1;
+    }
     return state;
 };
 
 const store = createStore(reducer);
+
+store.dispatch({ type: 'ADD'});
+store.dispatch({ type: 'ADD'});
+store.dispatch({ type: 'ADD'});
+store.dispatch({ type: 'ADD'});
+store.dispatch({ type: 'ADD'});
+store.dispatch({ type: 'ADD'});
+store.dispatch({ type: 'MINUS'});
 
 console.log(store.getState());
