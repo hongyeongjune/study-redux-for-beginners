0b3258e33865e095190d8cc75359dcab0ed3e7bd
hongyeongjune <hongyeongjune@naver.com>
2022-10-13 21:55:58 +0900

    createSlice

--- redux/src/components/ToDo.js
+++ redux/src/components/ToDo.js
@@ -1,4 +1,4 @@
-import { actionCreators } from '../store';
+import { remove } from '../store';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
@@ -17,7 +17,7 @@ function ToDo({ text, deleteToDo, id }) {
 
 function mapDispatchToProps(dispatch, ownProps) {
     return {
-        deleteToDo: () => dispatch(actionCreators.deleteToDo(ownProps.id))
+        deleteToDo: () => dispatch(remove(ownProps.id))
     };
 }
 
