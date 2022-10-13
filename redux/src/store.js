855560c869f6b975d5ff068331e5994af0701dde
hongyeongjune <hongyeongjune@naver.com>
2022-10-13 21:50:45 +0900

    configureStore

--- redux/src/store.js
+++ redux/src/store.js
@@ -1,5 +1,4 @@
-import { createStore } from "redux";
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { configureStore, createAction, createReducer } from '@reduxjs/toolkit';
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
@@ -11,7 +10,7 @@ const reducer = createReducer([], {
     [deleteToDo]: (state, action) => state.filter(toDo => toDo.id !== action.payload)
 });
 
-const store = createStore(reducer);
+const store = configureStore({ reducer });
 
 export const actionCreators = {
     addToDo,
